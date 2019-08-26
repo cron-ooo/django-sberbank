9afdb83a53d4156d4838b78df4401924d47010e2
tufedtm <tufedtm@gmail.com>
2019-08-26 17:27:52 +0000

    Добавить названия для ссылок

--- sberbank/urls.py
+++ sberbank/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
+
 from sberbank import views
 
+app_name = 'sberbank'
+
 urlpatterns = [  # noqa: pylint=invalid-name
-    url('payment/callback', views.callback),
-    url('payment/success', views.redirect, {'kind': 'success'}),
-    url('payment/fail', views.redirect, {'kind': 'fail'}),
-    url('payment/status/(?P<uid>[^/]+)/', views.StatusView.as_view()),
-    url('payment/bindings/(?P<client_id>[^/]+)/', views.BindingsView.as_view()),
-    url('payment/binding/(?P<binding_id>[^/]+)/', views.BindingView.as_view()),
-    url('payment/history/(?P<client_id>[^/]+)/', views.GetHistoryView.as_view()),
+    url('payment/callback', views.callback name='callback'),
+    url('payment/success', views.redirect, {'kind': 'success'} name='success'),
+    url('payment/fail', views.redirect, {'kind': 'fail'} name='fail'),
+    url('payment/status/(?P<uid>[^/]+)/', views.StatusView.as_view() name='status'),
+    url('payment/bindings/(?P<client_id>[^/]+)/', views.BindingsView.as_view() name='bindings'),
+    url('payment/binding/(?P<binding_id>[^/]+)/', views.BindingView.as_view() name='binding'),
+    url('payment/history/(?P<client_id>[^/]+)/', views.GetHistoryView.as_view() name='history'),
 ]
