ba30f4d53d56cb5ed8538e1b4010a183c20ddafb
tufedtm <tufedtm@gmail.com>
2019-08-26 09:39:19 +0300

    Add package install requires

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 import setuptools
 
-setuptools.setup(name='django-sberbank',
-    version='0.2.30',
+setuptools.setup(
+    name='django-sberbank',
+    version='0.3',
     description='Django app for Sberbank payments',
     url='http://github.com/madprogrammer/django-sberbank',
     author='Sergey Anufrienko',
     author_email='sergey.anoufrienko@gmail.com',
     license='BSD',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'celery==4.3.*',
+        'django-braces==1.13.*',
+        'djangorestframework==3.10.*',
+        'jsonfield==2.0.*',
+        'requests==2.22.*',
+    ],
     platforms='any',
     zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
     ]
 )
-
